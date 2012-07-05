9da1a1ca74377179347ab600e300ca40a0da82a5
Hoang Anh Le <hoanganh.le@me.com>
2012-07-05 10:48:32 +0300

    Update master

--- install_ruby_via_rvm.sh
+++ install_ruby_via_rvm.sh
@@ -12,11 +12,10 @@ sudo apt-get install nodejs openjdk-6-jdk build-essential openssl libreadline6 l
 curl -L https://get.rvm.io | bash -s stable
 	
 # update rvm variables
-. "$HOME/.rvm/scripts/rvm"
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # This loads RVM into a shell session.' >> $HOME/.bashrc
+. "$HOME/.bashrc"
 
-# rvm is loaded into shell as a function
-# thus, press 'q' to continue the installation process
+# rvm is NOT loaded into shell as a function
 command rvm install 1.9.3
 
 # set ruby 1.9.3 as default use
