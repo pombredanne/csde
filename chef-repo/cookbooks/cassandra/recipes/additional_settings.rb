3ac354bb13e59db8ff452b9c85bff33e580ff0ae
Hoang Anh Le <me@lehoanganh.de>
2012-07-10 17:15:46 +0200

    one more try

--- chef-repo/cookbooks/cassandra/recipes/additional_settings.rb
+++ chef-repo/cookbooks/cassandra/recipes/additional_settings.rb
@@ -28,8 +28,8 @@
 execute 'echo "export JAVA_HOME=/usr/lib/jvm/java-6-sun" | sudo -E tee -a ~/.bashrc'
 execute 'echo "export JAVA_HOME=/usr/lib/jvm/java-6-sun" | sudo -E tee -a ~/.profile'
 
-execute 'sudo bash -c "ulimit -n 32768"'
-execute 'echo "* soft nofile 32768" | sudo tee -a /etc/security/limits.conf'
-execute 'echo "* hard nofile 32768" | sudo tee -a /etc/security/limits.conf'
-execute 'sync'
-execute 'echo 3 > /proc/sys/vm/drop_caches'
+# execute 'sudo bash -c "ulimit -n 32768"'
+# execute 'echo "* soft nofile 32768" | sudo tee -a /etc/security/limits.conf'
+# execute 'echo "* hard nofile 32768" | sudo tee -a /etc/security/limits.conf'
+# execute 'sync'
+# execute 'echo 3 > /proc/sys/vm/drop_caches'
