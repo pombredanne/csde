eba9bcb3192f73dbb3e326b1ed61664083de448c
Hoang Anh Le <hoanganh.le@me.com>
2012-07-16 18:47:58 +0300

    Update master

--- chef-repo/cookbooks/cassandra/recipes/configure_cluster.rb
+++ chef-repo/cookbooks/cassandra/recipes/configure_cluster.rb
@@ -1,7 +1,7 @@
 ruby_block "configure_cluster" do
   block do
     File.open(node[:cassandra][:configure_file],"w") do |file|
-      file << "create keyspace #{node[:cassandra][:keyspace]} with strategy_options = {replication_factor:#{node[:cassandra][:column_family]}} and placement_strategy = 'org.apache.cassandra.locator.SimpleStrategy';"
+      file << "create keyspace #{node[:cassandra][:keyspace]} with strategy_options = {replication_factor:#{node[:cassandra][:replication_factor]}} and placement_strategy = 'org.apache.cassandra.locator.SimpleStrategy';"
       file << "\n"
       file << "use #{node[:cassandra][:keyspace]};"
       file << "\n"
