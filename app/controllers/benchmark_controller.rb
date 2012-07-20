dc11ef9a321edc9c144473e7522f158da63ef808
Hoang Anh Le <me@lehoanganh.de>
2012-07-20 11:20:08 +0200

    getting benchmark profile from the given source

--- app/controllers/benchmark_controller.rb
+++ app/controllers/benchmark_controller.rb
@@ -6,13 +6,25 @@ class BenchmarkController < ApplicationController
     @status = ""
     
     benchmark_profile_url = params[:benchmark_profile_url]
-      
-    @status << benchmark_profile_url    
+    @status << benchmark_profile_url
+    
+    logger.debug "::: Parsing the benchmark profile..."
+    configuration_array = profile_parse benchmark_profile_url
+        
   end
   
   # parse the profile file to get configuration parameters
   private
-  def profile_parse
+  def profile_parse benchmark_profile_url
+    # contain all configuration parameters
+    configuration_array = []
     
+    logger.debug "::: Getting the benchmark profile from the given source..."
+    benchmark_profile_path = "#{Rails.root}/chef-repo/.chef/tmp/benchmark_profiles.yaml"
+    system "curl -L #{benchmark_profile_url} -o #{benchmark_profile_path}"
+    logger.debug "::: Getting the benchmark profile from the given source... [OK]"
+    
+    
+    configuration_array
   end
 end
