4d5d50a6cd4b9c159b88990b8cfb034b96eb19c1
Hoang Anh Le <me@lehoanganh.de>
2012-07-20 11:11:06 +0200

    benchmark profile

--- app/controllers/benchmark_controller.rb
+++ app/controllers/benchmark_controller.rb
@@ -2,9 +2,12 @@ require 'helper'
 class BenchmarkController < ApplicationController
   include Helper
   
-  
   def run
+    @status = ""
     
+    benchmark_profile_url = params[:benchmark_profile_url]
+      
+    @status << benchmark_profile_url    
   end
   
   # parse the profile file to get configuration parameters
