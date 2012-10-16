6fb0b1b5ccc9431a3edc77ed4af832466fcb9ad2
Hoang Anh Le <me@lehoanganh.de>
2012-10-16 11:25:29 +0200

    [try] try!

--- chef-repo/cookbooks/cassandra/recipes/additional_settings.rb
+++ chef-repo/cookbooks/cassandra/recipes/additional_settings.rb
@@ -2,6 +2,7 @@
 execute 'echo "export JAVA_HOME=/usr/lib/jvm/java-6-sun" | sudo -E tee -a ~/.bashrc'
 execute 'echo "export JAVA_HOME=/usr/lib/jvm/java-6-sun" | sudo -E tee -a ~/.profile'
 
+
 # use MX4J
 execute 'wget https://s3.amazonaws.com/kcsdb-init/mx4j-tools.jar'
 execute 'sudo cp /home/ubuntu/mx4j-tools.jar /usr/share/cassandra/lib'
