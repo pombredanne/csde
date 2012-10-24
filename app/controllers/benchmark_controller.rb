6658c1ee9983af57be195282a9c8121cb793dbb7
Hoang Anh Le <me@lehoanganh.de>
2012-10-24 16:25:12 +0200

    f

--- app/controllers/benchmark_controller.rb
+++ app/controllers/benchmark_controller.rb
@@ -2349,7 +2349,7 @@ class BenchmarkController < ApplicationController
     #puts "Changing jruby mode"
     #system ". /home/ubuntu/.rvm/scripts/rvm"
     #system "/bin/bash --login rvm use jruby"
-    system "jruby-1.6.8 -S #{requester_path}"
+    system "jruby -S #{requester_path}"
     #system "rvm --default use 1.9.3"
     #system "bash #{Rails.root}/chef-repo/.chef/sh/invoke_requester.sh"
   end
