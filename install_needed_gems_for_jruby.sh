678f6cba3107c08f8a0ac125e35b7b95db8583a9
Hoang Anh Le <me@lehoanganh.de>
2012-10-24 14:59:41 +0200

    f

---
+++ install_needed_gems_for_jruby.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+jruby-1.6.8 -S gem install logger -v '1.2.8'
+jruby-1.6.8 -S gem install fog -v '1.6.0'
+jruby-1.6.8 -S gem install chef -v '10.12.0'
+jruby-1.6.8 -S gem install net-ssh -v '2.2.2'
+jruby-1.6.8 -S gem install parallel -v '0.5.18'
+jruby-1.6.8 -S gem install rails -v '3.2.8'
