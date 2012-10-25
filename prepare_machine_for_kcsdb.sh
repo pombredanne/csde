48be224bcb6dc161869fd570c436b5aa2476b848
Hoang Anh Le <me@lehoanganh.de>
2012-10-25 11:13:53 +0200

    [bf] set env var with 'export'

--- prepare_machine_for_kcsdb.sh
+++ prepare_machine_for_kcsdb.sh
@@ -48,7 +48,7 @@ install_jruby(){
 	echo "::::::::::::::::::::::::::::::::::::::::::::"
 	wget https://s3.amazonaws.com/kcsdb-init/jruby-bin-1.7.0.tar.gz --output-document /home/ubuntu/jruby-bin-1.7.0.tar.gz --quiet
 	tar xf /home/ubuntu/jruby-bin-1.7.0.tar.gz
-	echo 'JRUBY_HOME=/home/ubuntu/jruby-1.7.0' | tee -a $HOME/.bashrc
+	echo 'export JRUBY_HOME=/home/ubuntu/jruby-1.7.0' | tee -a $HOME/.bashrc
 	echo 'PATH=$PATH:$JRUBY_HOME/bin' | tee -a $HOME/.bashrc
 }
 
