58c9fed17425d689fd9a2356fe7a8db18dab2ced
unknown <LHA@.(none)>
2012-11-02 16:40:56 +0100

    [update] use pbzip2

--- chef-repo/.chef/sh/create_snapshot.sh
+++ chef-repo/.chef/sh/create_snapshot.sh
@@ -4,6 +4,10 @@
 
 index=$1
 
+echo "::: Installing pbzip2..."
+sudo apt-get update -qq
+sudo apt-get install pbzip2 -qq
+
 echo "::: Installing s3cmd..."
 wget https://s3.amazonaws.com/kcsdb-init/s3cmd-1.1.0-beta3.tar.gz --output-document /home/ubuntu/s3cmd-1.1.0-beta3.tar.gz --quiet
 tar xf /home/ubuntu/s3cmd-1.1.0-beta3.tar.gz
@@ -17,5 +21,8 @@ sudo /etc/init.d/cassandra stop
 echo "::: Changing user for cassandra folder..."
 sudo chown -R ubuntu /var/lib/cassandra
 
+echo "::: Compressing backup files into cas-snap-$index.tar.bz2..."
+tar -cf cas-snap-$index.tar.bz2 -I /usr/bin/pbzip2 /var/lib/cassandra/data/usertable/data/snapshots/cassandra-snapshot/
+
 echo "::: Uploading all files to bucket 'kcsdb-init' in folder 'cassandra-$index'"
-/home/ubuntu/s3cmd-1.1.0-beta3/./s3cmd put /var/lib/cassandra/data/usertable/data/snapshots/cassandra-snapshot/* s3://kcsdb-init/cassandra-$index/
+/home/ubuntu/s3cmd-1.1.0-beta3/./s3cmd put cas-snap-$index.tar.bz2 s3://kcsdb-init
