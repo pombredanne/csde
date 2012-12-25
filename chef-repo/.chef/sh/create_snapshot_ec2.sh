18dbbdcc7be07babe87db0660151f35debda4514
unknown <Le Hoang Anh@.(none)>
2012-12-25 21:39:44 +0100

    update

--- chef-repo/.chef/sh/create_snapshot_ec2.sh
+++ chef-repo/.chef/sh/create_snapshot_ec2.sh
@@ -4,7 +4,7 @@ echo "::: Creating snapshot files of Cassandra..."
 nodetool -h localhost -p 7199 snapshot usertable -cf data -t cassandra-snapshot
 
 echo "::: Moving all snapshot data from RAID0 disks to EBS store"
-sudo mv /var/lib/cassandra/data/usertable/data/snapshots/cassandra-snapshot/ /home/ubuntu/
+sudo cp /var/lib/cassandra/data/usertable/data/snapshots/cassandra-snapshot/ /home/ubuntu/
 
 echo "::: Changing ownership back to ubuntu"
 sudo chown -R ubuntu:ubuntu /home/ubuntu/cassandra-snapshot
