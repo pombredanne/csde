a14338267798751ccc584e30095dd68f3558946d
Hoang Anh Le <me@lehoanganh.de>
2013-03-23 20:09:57 +0100

    [bf] rvmsudo can not detect sudoers

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+# fix the bug
+# Warning: can not check `/etc/sudoers` for `secure_path`, falling back to call via `/usr/bin/env`, 
+# this breaks rules from `/etc/sudoers`. export rvmsudo_secure_path=1 to avoid the warning.
+export rvmsudo_secure_path=1
+
 rvmsudo rails server thin
